ab21eb8d1d81fb6f8ab4824b0f33a682cfe0b8b4
xlfsummer <xlf-summer@outlook.com>
2019-04-27 17:39:56 +0800

    优化函数名

--- 2.3-7 is-sum-of-two-elements-exist/code.js
+++ 2.3-7 is-sum-of-two-elements-exist/code.js
@@ -1,7 +1,7 @@
 let mergeSort = require("../2.3 merge-sort/code")
 let binarySearch = require("../2.3-5 binary-search/code")
 
-function findSum(S, x){
+function isSumExist(S, x){
     mergeSort(S) // n lg n
     for (let i = 0; i < S.length; i ++){ 
         let result = binarySearch(x - S[i], S); // n * lg n
@@ -11,5 +11,5 @@ function findSum(S, x){
 }
 
 
-let result = findSum([4,2,5,1,63,24,64,13,90,42,46,12,32,61,94,36,7], 100);
+let result = isSumExist([4,2,5,1,63,24,64,13,90,42,46,12,32,61,94,36,7], 100);
 console.log(result);
